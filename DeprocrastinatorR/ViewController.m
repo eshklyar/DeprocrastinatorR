c2557c8551b8e762de3cc80ba134342506a9954f
eshklyar <eshklyar@gmail.com>
2015-05-06 17:28:08 -0500

    added colored priorites that work, clean version

--- DeprocrastinatorR/ViewController.m
+++ DeprocrastinatorR/ViewController.m
@@ -59,59 +59,20 @@
                 self.priority =1;
                 [self.prioritis replaceObjectAtIndex:path.row withObject:[NSNumber numberWithInteger:1]];
             }
-//            [self changeRowColor:path withPriority:self.priority];
-    //          NSLog(@"priority 2 %ld", self.priority);
         }
-
     }
     else{
-//        [self.array removeObjectAtIndex:self.selectedIndexPath.row];
         [self.prioritis removeObjectAtIndex:path.row];
         NSLog(@"path %ld", path.row);
-
     }
-//    if ([[self.tableView cellForRowAtIndexPath:path].backgroundColor isEqual:[UIColor whiteColor]]) {
-//        self.priority = 0;
-//    }
-//
-//else
-//    self.priority =1;
-
-
-
-//    self.swipePath = path;
-//    if (path) {
-//        NSLog(@"path");
-//         UITableViewCell * cell = (UITableViewCell *)[self.tableView cellForRowAtIndexPath:path];
-//        cell.textLabel.backgroundColor = [UIColor greenColor];
-//        cell.backgroundColor = [UIColor redColor];
-//        [self.tableView cellForRowAtIndexPath:path].backgroundColor = [UIColor blueColor];
-//
-//    }
-//    [self.tableView cellForRowAtIndexPath:path].textLabel.text =@"bla" ;
-//
-//
-//    [self.tableView cellForRowAtIndexPath:path].textLabel.backgroundColor = [UIColor grayColor];
-//    [self.tableView cellForRowAtIndexPath:path].detailTextLabel.text =@"bla";
-//     [self.tableView cellForRowAtIndexPath:path].accessoryType = UITableViewCellAccessoryCheckmark;
-
 
     [self.tableView reloadData];
-//    NSLog(@"swipe %ld", (long)path.row);
-
-//    self.swipe = YES;
-
-//    oldPath = path;
-
-
 }
 
 - (IBAction)tapped:(UITapGestureRecognizer *)gesture {
     CGPoint point = [gesture locationInView:self.tableView];
 
     self.selectedIndexPath=[self.tableView indexPathForRowAtPoint:point];
-
-
     if ((self.array.count !=0) && (self.checks.count !=0))
     {
         if (!self.edit)
@@ -127,15 +88,10 @@
             [self.checks removeObjectAtIndex:self.selectedIndexPath.row];
             [self.prioritis removeObjectAtIndex:self.selectedIndexPath.row];
             NSLog(@"%@",self.prioritis);
-             NSLog(@"%ld",self.selectedIndexPath.row);
-//            [self.tableView reloadData];
-
+            NSLog(@"%ld",self.selectedIndexPath.row);
         }
-
         [self markRowsWithCheck:self.selectedIndexPath];
-
     }
-
 }
 -(void)markRowsWithCheck:(NSIndexPath *)indexPath
 {
@@ -146,33 +102,6 @@
         [self.tableView cellForRowAtIndexPath:indexPath].accessoryType = UITableViewCellAccessoryNone;
 
      [self.tableView reloadData];
-}
--(void)changeRowColor:(NSIndexPath *)path withPriority:(NSInteger)priorityNumber
-
-{
-    switch (priorityNumber)
-    {
-        case 1:
-
-            [self.tableView cellForRowAtIndexPath:path].backgroundColor = [UIColor redColor];
-            break;
-
-        case 2:
-
-            [self.tableView cellForRowAtIndexPath:path].backgroundColor = [UIColor blueColor];
-            break;
-
-        case 3:
-
-            [self.tableView cellForRowAtIndexPath:path].backgroundColor = [UIColor greenColor];
-            break;
-
-        default:
-
-            [self.tableView cellForRowAtIndexPath:path].backgroundColor = [UIColor whiteColor];
-            break;
-    }
-    [self.tableView reloadData];
 }
 - (IBAction)onAddBtnPressed:(id)sender {
     [self.array addObject:self.textField.text];
@@ -210,7 +139,6 @@
         cell=[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
 
         cell.textLabel.text = [self.array objectAtIndex:indexPath.row];
-//    cell.textLabel.backgroundColor = [UIColor grayColor];
 
         if ([[self.checks objectAtIndex:indexPath.row] isEqualToNumber:[NSNumber numberWithBool:0]])
             cell.accessoryType = UITableViewCellAccessoryNone;
@@ -221,58 +149,21 @@
     NSNumber* value = [self.prioritis objectAtIndex:indexPath.row] ;
 
     NSInteger valueNumber = [value integerValue];
+
     switch (valueNumber)
     {
-        case 1:
-
-        cell.backgroundColor = [UIColor redColor];
+        case 1: cell.backgroundColor = [UIColor redColor];
             break;
 
-        case 2:
-
-        cell.backgroundColor = [UIColor blueColor];
+        case 2: cell.backgroundColor = [UIColor blueColor];
             break;
 
-        case 3:
-
-        cell.backgroundColor = [UIColor greenColor];
+        case 3: cell.backgroundColor = [UIColor greenColor];
             break;
 
-        default:
-
-        cell.backgroundColor = [UIColor whiteColor];
+        default: cell.backgroundColor = [UIColor whiteColor];
             break;
     }
-
-
-
-//    if ([[self.prioritis objectAtIndex:indexPath.row] isEqualToNumber:[NSNumber numberWithInteger:1]]) {
-//        cell.backgroundColor = [UIColor redColor];
-//    } else if ([[self.prioritis objectAtIndex:indexPath.row] isEqualToNumber:[NSNumber numberWithInteger:2]]){
-//        cell.backgroundColor = [UIColor blueColor];
-//    }else if ([[self.prioritis objectAtIndex:indexPath.row] isEqualToNumber:[NSNumber numberWithInteger:3]]){
-//        cell.backgroundColor = [UIColor greenColor];
-//    }else if ([[self.prioritis objectAtIndex:indexPath.row] isEqualToNumber:[NSNumber numberWithInteger:0]]){
-//        cell.backgroundColor = [UIColor whiteColor];
-//    }
-
-
-
-
-//    if (self.swipe) {
-//        cell.textLabel.backgroundColor =[UIColor greenColor];
-//    }
-
-//    while (self.prioritis)
-//    {
-//        NSLog(@"bla");
-//        NSLog(@"the count bla %ld", self.prioritis.count);
-
-
-
-//        [self.tableView reloadData];
-//    [self resetColors:indexPath];
-
 return cell;
 }
 
@@ -289,7 +180,6 @@ return cell;
     return YES;
 }
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
-//    [self.tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:self.selectedIndexPath] withRowAnimation:UITableViewRowAnimationLeft];
     [self.tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationLeft];
     [self.tableView selectRowAtIndexPath:indexPath animated:YES scrollPosition:UITableViewScrollPositionNone];
 
@@ -298,12 +188,11 @@ return cell;
 }
 
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
-    NSLog(@"didSelectRowAtIndexPath method is called");
 
+    NSLog(@"didSelectRowAtIndexPath method is called");
 
     self.selectedIndexPath = indexPath;
     if (self.edit) {
-//        [tableView cellForRowAtIndexPath:indexPath].accessoryType = UITableViewCellAccessoryCheckmark;
         [tableView cellForRowAtIndexPath:indexPath].textLabel.backgroundColor = [UIColor redColor];
 
         [tableView reloadData];
@@ -313,45 +202,21 @@ return cell;
 }
 -(void)tableView:(UITableView *)tableView didDeselectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-//    [tableView cellForRowAtIndexPath:indexPath].accessoryType = UITableViewCellAccessoryNone;
      NSLog(@"didDeselectRowAtIndexPath");
 }
 
 #pragma mark textFiled
 
 - (void)textFieldDidBeginEditing:(UITextField *)textField{
-//     NSLog(@"textFieldDidBeginEditing was called");
 
 }
 - (void)textFieldDidEndEditing:(UITextField *)textField {
-//    [self.tableView reloadData];
     self.textField.text = @"";
 }
 - (BOOL)textFieldShouldReturn:(UITextField *)textField{
 
-//    [self.array addObject:self.textField.text];
-//
     [self.textField resignFirstResponder];
-//    [self.tableView reloadData];
-
     return YES;
 }
 
--(void)resetColors:(NSIndexPath*)indexPath{
-    for (NSInteger x=0; x< self.prioritis.count; x++)
-    {
-        //            NSNumber *i = [NSNumber numberWithInteger:x];
-        NSNumber * z = self.prioritis[x];
-        NSInteger i = [z integerValue];
-
-        //            int j = (int)i;
-        [self changeRowColor:indexPath withPriority:i]  ;
-        NSLog(@"bla bla");
-        NSLog(@"the count bla bla %ld", self.prioritis.count);
-
-        
-    }
-
-
-}
 @end
